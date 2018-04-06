58278f3005e1133ccc9b319b083fa895453bf5c3
João Bordalo <joao.bordalo@gmail.com>
2018-04-06 15:52:22 +0100

    updates mongodb from 3.2 to 3.6

--- scripts/linux/install-mongodb.sh
+++ scripts/linux/install-mongodb.sh
@@ -9,8 +9,12 @@ set -e
 
 #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 #echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+if [[ `lsb_release -a 2> /dev/null` =~ Release:.*16\. ]]; then
+  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+else
+  echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+fi
 
 sudo apt-get update -y
 sudo apt-get install mongodb-org mongodb-org-server mongodb-org-shell mongodb-org-tools -y
