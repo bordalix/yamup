119847e0447ce76f26db118ac1d4cde70b414c24
João Bordalo <joao.bordalo@gmail.com>
2020-09-23 11:19:45 +0100

    add app to enabled systemd services, will start after reboot

--- scripts/linux/wrap-up.sh
+++ scripts/linux/wrap-up.sh
@@ -5,6 +5,7 @@ if test -f /etc/os-release; then
   sudo chown root /etc/systemd
   sudo chown root /etc/systemd/system
   sudo systemctl daemon-reload
+  sudo systemctl enable <%= appName %>
 else
   sudo chown root /etc/
   sudo chown root /etc/init
